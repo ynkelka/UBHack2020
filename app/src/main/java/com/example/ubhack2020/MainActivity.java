c11d672239a63f83bece3278db12894fb677f1b6
Ykelkar2 <ykelkar@buffalo.edu>
2020-10-24 15:53:57 -0400

    Get user input and display something

--- app/src/main/java/com/example/ubhack2020/MainActivity.java
+++ app/src/main/java/com/example/ubhack2020/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.ubhack2020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+    Button mButton;
+    EditText mEdit;
+    TextView mText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButton = findViewById(R.id.enter);
+
+        mButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                mEdit = findViewById(R.id.editText);
+                mText = findViewById(R.id.textView2);
+                mText.setText(mEdit.getText());
+
+            }
+        });
     }
+
+
+
+
 }
